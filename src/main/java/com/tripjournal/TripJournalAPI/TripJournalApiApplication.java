1cd4415da54634f045f0e36da038ecbb1fcf51d1
MariusMsw <mariusliviu70@yahoo.com>
2021-04-03 17:59:10 +0300

    add cors

--- src/main/java/com/tripjournal/TripJournalAPI/TripJournalApiApplication.java
+++ src/main/java/com/tripjournal/TripJournalAPI/TripJournalApiApplication.java
@@ -2,12 +2,32 @@ package com.tripjournal.TripJournalAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class TripJournalApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripJournalApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistrationBean() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
